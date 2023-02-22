5f1f846742e03712a06e968f1805e3ef896ec46c
Rodrigo Quaresma <rodrigo.quaresma.souza@gmail.com>
2023-02-22 18:27:21 -0300

    Finalizacao do front-end

---
+++ CadastroContatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CadastroContatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
